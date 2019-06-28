576d96b0f69c2822f230b74d82b61df921f94ca3
aro2019 <52208639+aro2019@users.noreply.github.com>
2019-06-28 11:17:55 +0200

    Update osiris.py

--- osiris.py
+++ osiris.py
@@ -1,6 +1,3 @@
-import os
-import token
-
 import discord
 from discord.ext import commands
 
@@ -15,30 +12,41 @@ async def on_ready():
 
 bot.remove_command('help')
 
-@bot.command()
-async def info(ctx):
-    embed = discord.Embed(title="osiris", description="bot du conseil", color=0xeee657)
+@bot.event
+async def connect(ctx):
+    id_chan_bot = bot.get_channel(593143209178431489)
+    if ctx.channel == id_chan_bot
+        @bot.command()
+        async def info(ctx):
+            embed = discord.Embed(title="osiris", description="bot du conseil", color=0xeee657)
 
-    # give info about you here
-    embed.add_field(name="dévellopeur", value="aro20")
+            # give info about you here
+            embed.add_field(name="dévellopeur", value="aro20")
 
-    # Shows the number of servers the bot is member of.
-    embed.add_field(name="serveur connecté avec osiris", value=f"{len(bot.guilds)}")
+            # Shows the number of servers the bot is member of.
+            embed.add_field(name="serveur connecté avec osiris", value=f"{len(bot.guilds)}")
 
-    # give users a link to invite thsi bot to their server
-    embed.add_field(name="Invite", value="non")
+            # give users a link to invite thsi bot to their server
+            embed.add_field(name="Invite", value="non")
 
-    embed.add_field(name="droits", value="administrateur")
+            embed.add_field(name="droits", value="administrateur")
 
-    await ctx.send(embed=embed)
+            await ctx.send(embed=embed)
 
-@bot.command()
-async def help(ctx): # faire que seul les rangs 4 et le chef y accede
-    embed = discord.Embed()
+        @bot.command()
+        async def help(ctx): # faire que seul les rangs 4 et le chef y accede
+            embed = discord.Embed()
 
-    embed.add_field(name="!info", value="Gives a little info about the bot", inline=False)
-    embed.add_field(name="!help", value="Gives this message", inline=False)
+            embed.add_field(name="!info", value="Gives a little info about the bot", inline=False)
+            embed.add_field(name="!help", value="Gives this message", inline=False)
 
-    await ctx.send(embed=embed)
+            await ctx.send(embed=embed)
 
+        @bot.command()
+        async def users(ctx):
+            id_serv = bot.get_guild(593129527417241636)
+            await ctx.send(f"""#of member: {id.member_count}""")
+    else:
+        await ctx.send("le salon n est pas le bon")
+    
 bot.run("NTkzNDQ5MTQyNzc3NDEzNjM3.XRXRKQ.beZQO8iVlxKT7WzkEs7SWBZgA9g")
