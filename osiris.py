b51180c6fdd1efaa50d215b0bb2a5aebb9bc7d67
aro2019 <52208639+aro2019@users.noreply.github.com>
2019-07-02 17:20:26 +0200

    Update osiris.py

--- osiris.py
+++ osiris.py
@@ -11,38 +11,36 @@ async def on_ready():
     print('-----------------------')
 
 bot.remove_command('help')
-
-@bot.event
-async def connect(ctx):
-    @bot.command()
-    async def info(ctx):
-        embed = discord.Embed(title="osiris", description="bot du conseil", color=0xeee657)
-
-        # give info about you here
-        embed.add_field(name="dévellopeur", value="aro20")
-
-        # Shows the number of servers the bot is member of.
-        embed.add_field(name="serveur connecté avec osiris", value=f"{len(bot.guilds)}")
-
-        # give users a link to invite thsi bot to their server
-        embed.add_field(name="Invite", value="non")
-
-        embed.add_field(name="droits", value="administrateur")
-
-        await ctx.send(embed=embed)
-
-    @bot.command()
-    async def help(ctx): # faire que seul les rangs 4 et le chef y accede
-        embed = discord.Embed()
-
-        embed.add_field(name="!info", value="Gives a little info about the bot", inline=False)
-        embed.add_field(name="!help", value="Gives this message", inline=False)
-
-        await ctx.send(embed=embed)
-
-    @bot.command()
-    async def ban(ctx, members= commands.Greedy[discord.Member], delete_days: typing.Optional[int] = 0, *, reason: str):
-        for member in members:
-            await member.ban(delete_message_days=delete_days, reason=reason)
     
-bot.run("NTkzNDQ5MTQyNzc3NDEzNjM3.XRXRKQ.beZQO8iVlxKT7WzkEs7SWBZgA9g")
+@bot.command()
+async def info(ctx):
+    embed = discord.Embed(title="osiris", description="bot du conseil", color=0xeee657)
+
+    # give info about you here
+    embed.add_field(name="dévellopeur", value="aro20")
+
+    # Shows the number of servers the bot is member of.
+    embed.add_field(name="serveur connecté avec osiris", value=f"{len(bot.guilds)}")
+
+    # give users a link to invite thsi bot to their server
+    embed.add_field(name="Invite", value="non")
+
+    embed.add_field(name="droits", value="administrateur")
+
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def help(ctx): # faire que seul les rangs 4 et le chef y accede
+    embed = discord.Embed()
+
+    embed.add_field(name="!info", value="Gives a little info about the bot", inline=False)
+    embed.add_field(name="!help", value="Gives this message", inline=False)
+
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def ban(ctx, members= commands.Greedy[discord.Member], delete_days: typing.Optional[int] = 0, *, reason: str):
+    for member in members:
+        await member.ban(delete_message_days=delete_days, reason=reason)
+
+bot.run("NTkzNDQ5MTQyNzc3NDEzNjM3.XRt1pg.mb0jSeBGyBWIoZkucOOVsywqHig")
